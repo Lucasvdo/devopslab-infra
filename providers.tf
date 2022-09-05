8dd5a70e9b7a277ce2674f1af2fe2c949eb3f3a8
Lucas Valerio <lucasvalerio@live.com>
2022-09-05 19:51:34 -0300

    Alteração dos arquivos para provisionar o APPENGINE

--- providers.tf
+++ providers.tf
@@ -10,9 +10,7 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/gcpkey.json")
-
   project = "teste-351601"
-  region  = "us-west1"
-  zone    = "us-west1-b"
+  region  = "us-east4"
+  zone    = "us-east4-b"
 }
