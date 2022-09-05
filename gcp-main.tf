a8a5c0282b5e3810f30f06287411ba941b27136c
Lucas Valerio <lucasvalerio@live.com>
2022-09-05 19:35:19 -0300

    Change Zone

--- gcp-main.tf
+++ gcp-main.tf
@@ -2,7 +2,7 @@
 resource "google_compute_instance" "firstvm" {
   name         = "helloworld"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
+  zone         = "e2-micro"
 
   # Defini a Imagem da VM
   boot_disk {
